c47afaddad7ad567d1956b0d00faa091269a673a
managed86 <lover@list.ru>
2020-12-14 23:13:44 +0300

    about, login, terms +adaptive-index

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,28 @@
 $(function() {
 
-    $('.slider-top__inner').slick({
-        dots: true,
-        arrows: false
 
-    })
-    var mixer = mixitup('.products__items', {
-        selectors: {
-            control: '.products__btn'
-        }
-    });
-    var mixer = mixitup('.news__items', {
-        selectors: {
-            control: '.news__btn'
-        }
-    });
+   $('.slider-top__inner').slick({
+      dots: true,
+      arrows: false
+
+   })
+
+   $('.menu__burger').on('click', function(event) {
+      $('.menu__burger, .menu__list').toggleClass('active');
+   });
+
+
+
+   var mixer = mixitup('.products__items', {
+      selectors: {
+         control: '.products__btn'
+      }
+   });
+   var mixer = mixitup('.news__items', {
+      selectors: {
+         control: '.news__btn'
+      }
+   });
+
+
 });
